b133cf06849f61bf953420603799873d60b0816c
Brehcore <brehcore@gmail.com>
2025-11-24 15:11:22 -0300

    feat: Configurar uso de fuso horário e formato de data padrão
    - Adicionar configuração de fuso horário para "America/Sao_Paulo" no método `init` em `ApiApplication`.
    - Atualizar `application.properties` para especificar formato de data padrão `yyyy-MM-dd HH:mm:ss` e fuso horário.

--- API/src/main/java/com/gotree/API/ApiApplication.java
+++ API/src/main/java/com/gotree/API/ApiApplication.java
@@ -1,10 +1,13 @@
 package com.gotree.API;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
@@ -20,6 +23,12 @@ public class ApiApplication {
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
+	}
+
+	@PostConstruct
+	public void init() {
+		// Força a aplicação a usar o fuso horário de Brasília
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 	}
 
 }
